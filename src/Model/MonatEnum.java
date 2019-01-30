587e0382effb15fb5d0d208edf803094d4f0f27b
grischu <diavista@gmail.com>
2019-01-30 20:54:27 +0100

    änderungen für kleinen bildschirm und eclipse v2

--- src/Model/MonatEnum.java
+++ src/Model/MonatEnum.java
@@ -3,7 +3,7 @@ package Model;
 import com.sun.istack.internal.Nullable;
 
 public enum MonatEnum {
-    Januar(1), Februar(2), März(3), April(4), Mai(5), Juni(6),
+    Januar(1), Februar(2), Maerz(3), April(4), Mai(5), Juni(6),
     Juli(7), August(8), September(9), Oktober(10), November(11), Dezember(12);
 
     private int id;
